3fba58252f8d1065658780494b9c854bc984138b
Kaan <kaanozarslan@ogr.eskisehir.edu.tr>
2026-06-15 12:19:07 +0300

    impl: Health.cs (#3)

---
+++ CF-Core/Models/PlayerComponents/Health.cs
@@ -0,0 +1,32 @@
+namespace CF_Core.Models.PlayerComponents;
+
+public class Health
+{
+    private int MaxHealth { get; set; }
+    private int CurrentHealth { get; set; }
+
+    private Health(){}
+
+    public Health(int maxHealth)
+    {
+        this.MaxHealth = maxHealth;
+        this.CurrentHealth = maxHealth;
+    }
+
+    public Health(int maxHealth, int currentHealth)
+    {
+        this.MaxHealth = maxHealth;
+        this.CurrentHealth = currentHealth;
+    }
+
+    public static Health Default()
+    {
+        var health = new Health
+        {
+            MaxHealth = 100,
+            CurrentHealth = 100
+        };
+
+        return new Health();
+    }
+}
